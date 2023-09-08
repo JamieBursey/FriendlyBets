73d80eb2bd3302c4893216ad162df7e55b240f9c
Jamie Bursey <121978068+JamieBursey@users.noreply.github.com>
2023-09-08 14:25:23 -0230

    start rough layout

--- src/App.js
+++ src/App.js
@@ -6,23 +6,49 @@ import './App.css';
 function App() {
   return (
 
-    <div className="App text-bg-success p-3">
-      <header className="App-header">
+
+    <header className="App-header">
+      <div className="App text-bg-success p-3">
         <div class="container text-center">
           <div class="row">
             <div class="col">
-              Column
+              <button type="button" class="btn btn-danger">Fighting</button>
             </div>
             <div class="col">
-              Column
+              <button type="button" class="btn btn-primary">Hockey</button>
             </div>
             <div class="col">
-              Column
+              <button type="button" class="btn btn-warning">Basketball</button>
             </div>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+      <div class="text-bg-secondary p-3">
+        <div class="container text-center">
+          <div class="row">
+            <div class="col">
+              1 of 2
+            </div>
+            <div class="col">
+              2 of 2
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              1 of 3
+            </div>
+            <div class="col">
+              2 of 3
+            </div>
+            <div class="col">
+              3 of 3
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
   );
 }
 
