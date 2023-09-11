6ce0838002affc2bdaa8cd668773206e31db4f23
Apple <apple@Apples-MacBook-Pro.local>
2023-09-11 19:02:39 -0230

    black screen when dynamically generating buttons
    when trying to .map receiving error message undefined(reading map)

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import logo from './header.jpeg'
 
-
+function buttons({ sportButtons }) {
+  return (
+    <div className="App text-bg-success p-3 mb-3">
+      <div class="container text-center">
+        <div class="row">
+          {sportButtons.map((button, index) => (
+            <div class="col" key={index}>
+              <button type="button" className={`btn ${button.color}`}>{button.sport}</button>
+            </div>))}
+        </div>
+      </div>
+    </div >)
+}
 
 
 function App() {
+  const sportButtons = [
+    { sport: "Fighting", color: "btn-danger" },
+    { sport: "Hockey", color: "btn-light" },
+    { sport: "Football", color: "btn-warning" }
+  ]
   return (
 
     <>
       <header className="App-header">
-        return <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" />
         <div class="text-center fs-1 text-success text-bg-dark p-3">Friendly Bets</div>
         <div className="App text-bg-success p-3 mb-3">
           <div class="container text-center">
-            {buttons()}
+            {buttons(sportButtons)}
           </div>
         </div>
         <div class="App text-bg-success p-3">
@@ -78,24 +95,5 @@ function App() {
     </>
   );
 }
-
-function buttons() {
-  return (
-    <div className="App text-bg-success p-3 mb-3">
-      <div class="container text-center">
-        <div class="row">
-          <div class="col">
-            <button type="button" class="btn btn-danger">Fighting</button>
-          </div>
-          <div class="col">
-            <button type="button" class="btn btn-primary">Hockey</button>
-          </div>
-          <div class="col">
-            <button type="button" class="btn btn-warning">Basketball</button>
-          </div>
-        </div>
-      </div>
-    </div>)
-}
 export default App;
-export { buttons }
+
