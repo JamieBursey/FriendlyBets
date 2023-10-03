f07ce24706889e5016c4a69a530743ce50bf7b5e
Apple <apple@Apples-MacBook-Pro.local>
2023-10-03 17:25:08 -0230

    fixed mobile buttons
    fixed issue with buttons shifting on small screen

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,123 +1,88 @@
 import React from "react";
 import { renderFriends } from "../Data";
 function buttons(buttonsData) {
-    return (
-        <div className="App p-3 mb-3">
-            <div className="container text-center">
-                <div className="row">
-                    {buttonsData ? (
-                        buttonsData.map((button, index) => (
-                            <div className="col" key={index}>
-                                <button type="button" className={`btn ${button.color}`}>
-                                    {button.text}
-                                </button>
-                            </div>
-                        ))
-                    ) : null}
+  return (
+    <div className="App p-3 mb-3">
+      <div className="container text-center">
+        <div className="row">
+          {buttonsData
+            ? buttonsData.map((button, index) => (
+                <div className="col" key={index}>
+                  <button type="button" className={`btn ${button.color}`}>
+                    {button.text}
+                  </button>
                 </div>
-            </div>
+              ))
+            : null}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 function AddButtons() {
-    const sportButtons = [
-        { text: "Fighting", color: "btn-danger" },
-        { text: "Hockey", color: "btn-light" },
-        { text: "Football", color: "btn-warning" },
-        { text: "Baseball", color: "btn-primary" },
-    ];
-    // const [playerName, setPlayerName] = useState("");
+  const sportButtons = [
+    { text: "Fighting", color: "btn-danger" },
+    { text: "Hockey", color: "btn-light" },
+    { text: "Football", color: "btn-warning" },
+    { text: "Baseball", color: "btn-primary" },
+  ];
+  // const [playerName, setPlayerName] = useState("");
 
-    // const [newPlayer, setNewPlayer] = useState([]);
-    // const [playerRemoved, setPlayerRemoved] = useState("");
+  // const [newPlayer, setNewPlayer] = useState([]);
+  // const [playerRemoved, setPlayerRemoved] = useState("");
 
-    // const addPlayer = () => {
-    //     if (playerName.trim() !== "") {
-    //         setNewPlayer([...newPlayer, playerName]);
-    //         setPlayerName("");
-    //         console.log(newPlayer);
-    //     }
-    // };
+  // const addPlayer = () => {
+  //     if (playerName.trim() !== "") {
+  //         setNewPlayer([...newPlayer, playerName]);
+  //         setPlayerName("");
+  //         console.log(newPlayer);
+  //     }
+  // };
 
-    // const deletePlayer = () => {
-    //     const updatePlayers = newPlayer.filter(
-    //         (person) => person !== playerRemoved
-    //     );
-    //     setNewPlayer(updatePlayers);
-    //     setPlayerRemoved("");
-    // };
+  // const deletePlayer = () => {
+  //     const updatePlayers = newPlayer.filter(
+  //         (person) => person !== playerRemoved
+  //     );
+  //     setNewPlayer(updatePlayers);
+  //     setPlayerRemoved("");
+  // };
 
-    // const PlayerElement = () => {
-    //     return newPlayer.map((person) => (
-    //         <div key={person} className="col fs-4 text-danger">
-    //             {person}
-    //         </div>
-    //     ));
-    // };
-    const ButtonsBackground = {
-        backgroundColor: "#0B1305",
-        borderRadius: "5px"
-    }
-    const ButtonsStyle = {
-        width: "120px",
-        height: "35px",
-        fontSize: ".7em",
-        fontWeight: "bold"
-    }
-    return (
-        <>
-            <div style={ButtonsBackground} className="App p-3 mb-3">
-                <div className="container text-center">
-                    {buttons(sportButtons)}
-                </div>
-            </div>
-            <div style={ButtonsBackground} className="App p-3">
-                <div className="container text-center">
-                    <div className="d-grid gap-2 col-3 mx-auto">
-
-                        <div className="d-flex col-6 col-md-3">
-                            <input
-                                className=""
-                                type="text"
-                                placeholder="Add Friend"
-                            // value={playerName}
-                            // onChange={(event) => setPlayerName(event.target.value)}
-                            ></input>
-                            <button
-                                style={ButtonsStyle}
-                                className="btn btn-primary"
-                                type="button"
-                            >
-                                Add Friend
-                            </button>
-                        </div>
-                        <div className="d-flex col-6 col-md-3">
-                            <input
-                                className=""
-                                type="text"
-                                placeholder="Remove Friend"
-                            // value={playerRemoved}
-                            // onChange={(event) => setPlayerRemoved(event.target.value)}
-                            ></input>
-                            <button
-                                style={ButtonsStyle}
-                                className="btn btn-primary"
-                                type="button"
-                            // onClick={deletePlayer}
-                            >
-                                Remove Friend
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="row mb-3">{renderFriends()}</div>
-                </div>
-            </div>
-        </>
-    );
+  // const PlayerElement = () => {
+  //     return newPlayer.map((person) => (
+  //         <div key={person} className="col fs-4 text-danger">
+  //             {person}
+  //         </div>
+  //     ));
+  // };
+  const ButtonsBackground = {
+    backgroundColor: "#0B1305",
+    borderRadius: "5px",
+  };
+  const ButtonsStyle = {
+    width: "120px",
+    height: "35px",
+    fontSize: ".7em",
+    fontWeight: "bold",
+  };
+  return (
+    <>
+      <div style={ButtonsBackground} className="App p-3 mb-3">
+        <div className="container text-center">{buttons(sportButtons)}</div>
+      </div>
+      <div style={ButtonsBackground} className="App p-3">
+        <div className="container">
+        <div className="col-sm-6 col-md-6 col-lg-5">
+            <input type="text" placeholder="Add Friend"></input>
+            <button className="btn btn-primary btn-sm">Submit</button>
+            <input type="text" placeholder="Remove Friend"></input>
+            <button className="btn btn-primary btn-sm">Submit</button>
+        </div>
+        </div>
+          <div className="row mb-3">{renderFriends()}</div>
+        </div>
+    </>
+  );
 }
 
 export default AddButtons;
-
-
