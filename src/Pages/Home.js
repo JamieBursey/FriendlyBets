a4e86877048f65cb3143bc3d97a273d6fe3af76c
Apple <apple@Apples-MacBook-Pro.local>
2023-10-23 16:26:25 -0230

    fixed logged in check
    fixed logged in check so it does not open the home page if no user is logged in

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,25 @@
 import { Banner, Buttons, Matches } from "../Components";
 import { loggedInUserKey } from "../Data";
 import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
 
 function Home() {
   const checkIfLoggedInExists = localStorage.getItem(loggedInUserKey);
   const navigate = useNavigate();
-  console.log("checkLogin", checkIfLoggedInExists);
-  if (checkIfLoggedInExists == null) {
-    navigate("/login");
-    console.log("nav", navigate);
-  } else
-    return (
-      <div>
-        <Banner />
-        <Buttons />
-        <Matches />
-      </div>
-    );
+
+  useEffect(() => {
+    console.log("checkLogin", checkIfLoggedInExists);
+    if (!checkIfLoggedInExists || checkIfLoggedInExists === "null") {
+      navigate("/login");
+    }
+  }, [checkIfLoggedInExists, navigate]);
+  return (
+    <div>
+      <Banner />
+      <Buttons />
+      <Matches />
+    </div>
+  );
 }
 
 export default Home;
