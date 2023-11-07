2a2bc12e40dc4a940533fe4a10b8bc83bf96f679
Apple <apple@Apples-MacBook-Pro.local>
2023-11-07 18:18:12 -0330

    api down so doing styling
    NHL api temporarily down so styling some while it comes back

--- src/Pages/BetPage.js
+++ src/Pages/BetPage.js
@@ -1,30 +1,46 @@
 import { useEffect } from "react";
 import { BettingOptions } from "../Data";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { loggedInUserKey } from "../Data";
 
 const BetPage = () => {
-  const fetchTest = async () => {
-    const fetchGame = await fetch(
-      "https://statsapi.web.nhl.com/api/v1/schedule"
-    );
-    const gameData = await fetchGame.json();
-    const homeID = gameData.dates[0].games.map(
-      (game) => game.teams.home.team.id
-    );
-    for (let id of homeID) {
-      const teamFetch = await fetch(
-        `https://statsapi.web.nhl.com/api/v1/teams/${id}/roster`
-      );
-      const teamRoster = await teamFetch.json();
-      console.log(`Roster`, teamRoster);
-    }
-  };
-  useEffect(() => {
-    fetchTest();
-  });
+  const navigate = useNavigate();
+  //   const fetchTest = async () => {
+  //     const fetchGame = await fetch(
+  //       "https://statsapi.web.nhl.com/api/v1/schedule"
+  //     );
+  //     const gameData = await fetchGame.json();
+  //     const homeID = gameData.dates[0].games.map(
+  //       (game) => game.teams.home.team.id
+  //     );
+  //     for (let id of homeID) {
+  //       const teamFetch = await fetch(
+  //         `https://statsapi.web.nhl.com/api/v1/teams/${id}/roster`
+  //       );
+  //       const teamRoster = await teamFetch.json();
+  //       console.log(`Roster`, teamRoster);
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     fetchTest();
+  //   });
+  const loggedInUserData = localStorage.getItem(loggedInUserKey);
+  const LoggedInUsr = loggedInUserData ? JSON.parse(loggedInUserData) : null;
+  console.log(LoggedInUsr);
+  const usersFriendList = LoggedInUsr.friends;
+  console.log(usersFriendList);
   return (
-    <div className="text-white">
-      <p>Hello</p>
+    <div className="card mt-3" style={{ width: "90%" }}>
+      <div className="card-body">
+        <h5 className="card-title">Team vs Team</h5>
+        <p className="card-text">{usersFriendList}</p>
+        <a href="#" className="btn btn-primary mx-1">
+          Place Bet
+        </a>
+        <a href="#" className="btn btn-primary" onClick={() => navigate("/")}>
+          Go Back
+        </a>
+      </div>
     </div>
   );
 };
