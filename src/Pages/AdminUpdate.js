b52136e0a044e9d660e471960bedc3263024e04b
Apple <apple@apples-mbp.home>
2024-01-19 15:35:07 -0330

    styling changes

--- src/Pages/AdminUpdate.js
+++ src/Pages/AdminUpdate.js
@@ -28,6 +28,7 @@ const EditUserAsAdmin = () => {
   const [password, setPassword] = useState(userToUpdate.password || "");
   const [email, setEmail] = useState(userToUpdate.email || "");
   const [isAdmin, setIsAdmin] = useState(userToUpdate.isAdmin || false);
+
   return (
     <div className="container mt-3 text-center">
       <h1 className="text-info">Update User</h1>
@@ -38,7 +39,7 @@ const EditUserAsAdmin = () => {
         </span>
         <input
           type="text"
-          className="form-control"
+          className="form-control bg-secondary border-secondary text-info"
           defaultValue={userToUpdate.username}
           onChange={(event) => setUsername(event.target.value)}
           aria-label="Sizing example input"
@@ -52,7 +53,7 @@ const EditUserAsAdmin = () => {
         </span>
         <input
           type="text"
-          className="form-control"
+          className="form-control bg-secondary border-secondary text-info"
           defaultValue={userToUpdate.email}
           onChange={(event) => setEmail(event.target.value)}
           aria-label="Sizing example input"
@@ -61,12 +62,12 @@ const EditUserAsAdmin = () => {
       </div>
       <h5>Password Change</h5>
       <div className="input-group mb-3">
-        <span className="input-group-text" id="inputGroup-sizing-default">
+        <span className="input-group-text " id="inputGroup-sizing-default">
           Password
         </span>
         <input
           type="text"
-          className="form-control"
+          className="form-control bg-secondary border-secondary text-info"
           defaultValue={userToUpdate.password}
           onChange={(event) => setPassword(event.target.value)}
           aria-label="Sizing example input"
