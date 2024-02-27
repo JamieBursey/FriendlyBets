76e1bfd5704c1dd523e6e051c3d3276ec058a538
Apple <jamie4551@gmail.com>
2024-02-27 11:35:41 -0330

    getting some display
    getting some display but this api doesnt have team logos.

--- src/Components/mlb.js/mlbSchedule.js
+++ src/Components/mlb.js/mlbSchedule.js
@@ -1,46 +1,73 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const MlbSchedule = () => {
-  const fetchSchedule = async () => {
+const MlbTodaysGames = () => {
+  const [todaysGameArr, setTodaysGameArr] = useState([]);
+  const todaysSchedule = async () => {
     const response = await fetch(
       "https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1"
     );
     const teamData = await response.json();
+    let gamesHtml = [];
     const teamLogos = [];
     console.log(teamData);
-    console.log(response, "response");
+
+    setTodaysGameArr(teamData.dates[0].games);
   };
   useEffect(() => {
-    fetchSchedule();
+    todaysSchedule();
   }, []);
-  const createGameCard = () => {
-    return (
-      <div className="col-3 card m-1" style={{ width: "20rem" }}>
-        <div className="card-body">
-          Game Title {/* <h5 className="card-title">{gameTitle}</h5> */}
-          <div className="row">
-            <div className="col">{/* <img src= alt=""></img> */}</div>
-            <div className="col">{/* <img src= alt="" /> */}</div>
-          </div>
-          <p>
-            Date and time here{" "}
-            {/* {gameDay} at {new Date(gameTime).toLocaleTimeString()} */}
-          </p>
-          <div className="row">
-            <div className="col">
-              <button
-                // onClick={() => actionBtnTwo(game_ID, gameTitle)}
-                className="btn btn-primary w-100"
-              >
-                Bet Friend
-              </button>
+
+  return (
+    <div className="text-white text-center">
+      <h1>Todays Games</h1>
+      <div className="row justify-content-center">
+        {todaysGameArr.length > 0 ? (
+          todaysGameArr.map((game) => (
+            <div
+              key={game.gamePk}
+              className="col-3 card m-1"
+              style={{ width: "20rem" }}
+            >
+              <div className="card-body">
+                <h5 className="card-title">
+                  {game.teams.away.team.name} vs {game.teams.home.team.name}
+                </h5>
+                <div className="row">
+                  <div className="col">
+                    {/* <img
+                      src={game.awayTeam.logo}
+                      alt={`${game.awayTeam.abbrev} logo`}
+                      className="img-fluid"
+                    /> */}
+                  </div>
+                  <div className="col">
+                    {/* <img
+                      src={game.homeTeam.logo}
+                      alt={`${game.homeTeam.abbrev} logo`}
+                      className="img-fluid"
+                    /> */}
+                  </div>
+                </div>
+                <div className="row"></div>
+                <p>{game.gameDate}</p>
+                {/* <button
+                  onClick={() => actionBtnOne(game)}
+                  className="btn btn-primary w-100"
+                >
+                  Bet Friends
+                </button> */}
+              </div>
             </div>
-          </div>
-        </div>
+          ))
+        ) : (
+          <p>No live games currently.</p>
+        )}
       </div>
-    );
-  };
-  return createGameCard();
+    </div>
+  );
 };
 
-export { MlbSchedule };
+const MlbSchedule = () => {
+  return MlbTodaysGames();
+};
+export { MlbTodaysGames, MlbSchedule };
