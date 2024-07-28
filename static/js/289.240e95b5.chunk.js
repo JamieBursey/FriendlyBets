7b0a44bbe53e98347b2bc9e5dde60c087634b1a6
Apple <jamie4551@gmail.com>
2024-07-28 17:35:29 -0230

    Updates

---
+++ static/js/289.240e95b5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfriendlybets=self.webpackChunkfriendlybets||[]).push([[289],{3289:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.240e95b5.chunk.js.map
