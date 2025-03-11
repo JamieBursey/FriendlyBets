f617f72e64e17f073836cb257969bb308999067c
Apple <jamie4551@gmail.com>
2025-03-11 15:06:38 -0230

    update to use themes on nhl games

--- src/Components/Matches.js
+++ src/Components/Matches.js
@@ -1,13 +1,36 @@
 import { TodaysGames, LiveGames } from "../Data";
 import { NflTodaySchedule, NflWeeklySchedule } from "./nfl.js/NflSchedule";
+import { useTheme } from "./theme/ThemeContext";
+
 function Matches() {
+  const {theme}=useTheme()
+  const containerStyle = {
+    backgroundColor:
+      theme === "light"
+        ? "#E0E0E0"
+        : theme === "dark"
+        ? "#1E1E1E"
+        : "transparent", // Use transparent for retro to allow gradient
+    background:
+      theme === "retro"
+        ? "linear-gradient(to bottom, #0B1305 0%, #00008B 100%)"
+        : "none", // Apply gradient for retro theme
+    color: theme === "light" ? "#000000" : "#FFFFFF",
+    borderRadius: "5px",
+    padding: "20px",
+  };
+
+  const hrStyle = {
+    backgroundColor: theme === "light" ? "#000000" : "#FFFFFF",
+    height: "2px",
+  };
   return (
     <div
       className="container text-center p-2"
-      style={{ backgroundColor: "#1E1E1E", borderRadius: "5px" }}
+      style={containerStyle}
     >
       {LiveGames()}
-      <hr style={{ backgroundColor: "white", height: "2px" }} />
+      <hr style={hrStyle} />
       {TodaysGames()}
     </div>
   );
